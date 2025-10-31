7fd01293d0bbb37362e831cd20a1a16344f47e71
Rubinzo <random.randint.nft@gmail.com>
2025-10-31 13:10:32 +0100

    Ruben_Pariente ej1 actualizado

--- Ruben_Pariente/src/ControlAcceso.java
+++ Ruben_Pariente/src/ControlAcceso.java
@@ -13,17 +13,25 @@ public static void main(String[] args) {
 public  static void pedir_edad(){
     boolean flag = true;
     String mensaje = "";
+    int intentos = 0;
     while(flag == true){
 
         int edad = sc.pedirNumero("Introduzca su edad: ");
-        if(edad < 0){
+        if(intentos == 3){
+            System.out.println(color.red + "Has intentado más de 3 veces vuelve a intentarlo en un rato"+ color.reset);
+            flag = false;
+            break;
+        } else if(edad < 0){
             mensaje = color.red + "La edad no puede ser negativa"+ color.reset;
+            intentos = intentos + 1;
 
         } else if (edad == 0) {
             mensaje =color.red + "La edad no puede ser cero"+ color.reset;
+            intentos = intentos + 1;
 
         }else if (edad < 18){
             mensaje =color.red + "Acceso denegado: menor de edad" +color.reset;
+            intentos = intentos + 1;
 
         } else if (edad > 65 && edad <= 120) {
             mensaje =color.green + "Acceso con beneficios para jubilados"+ color.reset;
@@ -31,6 +39,8 @@ public  static void pedir_edad(){
 
         } else if (edad > 120){
             mensaje = color.red +"No puedes tener más de 120 años"+ color.reset;
+            intentos = intentos + 1;
+
 
         }else{
             mensaje =color.green + "Acceso permitido"+ color.reset;
